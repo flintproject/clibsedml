bfc61ed363b5a9dda316484270a0031a8a6de19a
Takeshi Abe <tabe@fixedpoint.jp>
2023-06-22 22:23:59 +0900

    CI via GitHub Actions

--- flake.nix
+++ flake.nix
@@ -1,9 +1,14 @@
 {
   description = "A flake for building clibsedml";
 
-  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-22.11;
+  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-23.05;
 
-  outputs = { self, nixpkgs }: {
+  inputs.sed-ml = {
+    url = github:SED-ML/sed-ml/44511c318bcf93c43256ac201905ace1f7675e4c;
+    flake = false;
+  };
+
+  outputs = { self, nixpkgs, sed-ml }: {
 
     packages.x86_64-linux.default =
       with import nixpkgs { system = "x86_64-linux"; };
@@ -14,10 +19,10 @@
         buildInputs = [ libxml2 ];
         src = self;
         configurePhase = "autoreconf -i -f && ./configure --prefix=$out";
-        buildPhase = "make";
-        # doCheck = true; # submodule sed-ml is unavailable
-        checkPhase = "make check";
-        installPhase = "make install";
+        preCheck = ''
+          ln -s ${sed-ml} sed-ml
+        '';
+        doCheck = true;
       };
 
     packages.aarch64-darwin.default =
@@ -29,10 +34,10 @@
         buildInputs = [ libxml2 ];
         src = self;
         configurePhase = "autoreconf -i -f && ./configure --prefix=$out";
-        buildPhase = "make";
-        # doCheck = true; # submodule sed-ml is unavailable
-        checkPhase = "make check";
-        installPhase = "make install";
+        preCheck = ''
+          ln -s ${sed-ml} sed-ml
+        '';
+        doCheck = true;
       };
   };
 }
